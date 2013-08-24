075af35615a992a6b31461fd92fe1a719229a37a
Shawn Sandy <shawnsandy04@gmail.com>
2013-08-24 08:03:14 -0400

    Added theme config.php Modified index.html -- config variables

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * TODO - The file description. *
+ * @package TODO
+ * @subpackage TODO
+ * @since TODO
+ * @author Shawn Sandy <shawnsandy04@gmail.com>
+ */
+
+/*
+ * Sample theme config
+ */
+$theme_config['copyright'] = '&copy Shawn Sandy 2013';
+$theme_config['contact_name'] = 'Full name';
+$theme_config['contact_address'] = 'Street City State';
+$theme_config['contact_number'] = '000 000 0000';
+$theme_config['facebook_page'] = 'shawnjsandy';
+$theme_config['twitter_username'] = 'shawnsandy';
+
